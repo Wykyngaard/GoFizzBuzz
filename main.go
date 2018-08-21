31e7977dd35cdd6e59d3d8b9051d91dcfbe23d09
Wykyngaard <32488277+Wykyngaard@users.noreply.github.com>
2018-08-21 15:28:31 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+/* FizzBuzz - prints every number 1 thru 15,
+numbers divisible by 3 prints Fizz,
+numbers divisible by 5 prints Buzz,
+and numbers divisible by both prints FizzBuzz */
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println(i, " FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println(i, " Fizz")
+		} else if i%5 == 0 {
+			fmt.Println(i, " Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
